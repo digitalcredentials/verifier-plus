fff0204121919798888ec1dc21604f9693d97dc6
James Chartrand <jc.chartrand@gmail.com>
2025-08-26 17:55:43 -0400

    add tests using fixtures repo VCs

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -8,7 +8,6 @@ test("should navigate to the about page", async ({ page }) => {
 //  await page.click("text=About");
   // The new URL should be "/about" (baseURL is used there)
 //  await expect(page).toHaveURL("/about");
-  // The new page should contain an h1 with "About"
   await expect(page.locator("h1")).toContainText("VerifierPlus");
     await page.getByTestId('vc-text-area').fill(testVC)
    //await page.getByTestId('verify-btn').click()
@@ -17,3 +16,40 @@ test("should navigate to the about page", async ({ page }) => {
 
 
 });
+
+test("pull from fixtures repo", async ({page}) => {
+  const response = await fetch("https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v1/dataIntegrityProof/didKey/legacy-noStatus-noExpiry.json");
+  const remoteTestVC = await response.text()
+  await page.goto("/");
+  await page.getByTestId('vc-text-area').fill(remoteTestVC)
+  await page.getByRole('button', { name: 'Verify' }).click()
+  await expect(page.getByText('no expiration date set')).toBeVisible();
+})
+
+test("revoked and expired", async ({page}) => {
+  const response = await fetch("https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v2/ed25519/didWeb/legacy-revokedStatus-expired.json");
+  const remoteTestVC = await response.text()
+  await page.goto("/");
+  await page.getByTestId('vc-text-area').fill(remoteTestVC)
+  await page.getByRole('button', { name: 'Verify' }).click()
+  await expect(page.getByText('has expired')).toBeVisible();
+  await expect(page.getByText('has been revoked')).toBeVisible();
+})
+
+test("in test registry", async ({page}) => {
+  const response = await fetch("https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v2/ed25519/didWeb/legacy-revokedStatus-expired.json");
+  const remoteTestVC = await response.text()
+  await page.goto("/");
+  await page.getByTestId('vc-text-area').fill(remoteTestVC)
+  await page.getByRole('button', { name: 'Verify' }).click()
+  await expect(page.getByText('DCC Sandbox Registry')).toBeVisible();
+})
+
+test("issued by test issuer", async ({page}) => {
+  const response = await fetch("https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v2/ed25519/didWeb/legacy-revokedStatus-expired.json");
+  const remoteTestVC = await response.text()
+  await page.goto("/");
+  await page.getByTestId('vc-text-area').fill(remoteTestVC)
+  await page.getByRole('button', { name: 'Verify' }).click()
+  await expect(page.getByText('DCC did:web for testing')).toBeVisible();
+})
