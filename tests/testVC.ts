6d3977ec68e6e22a551224a6491fb8b323840c41
James Chartrand <jc.chartrand@gmail.com>
2025-08-26 14:56:52 -0400

    add test VC

---
+++ tests/testVC.ts
@@ -0,0 +1,22 @@
+export default `{
+    "@context": [
+        "https://www.w3.org/ns/credentials/v2"
+    ],
+    "id": "urn:uuid:4636EB2F-1B73-47EA-981E-CFA85A5B13E3",
+    "type": [
+        "VerifiableCredential"
+    ],
+    "issuer": "did:key:z6MknNQD1WHLGGraFi6zcbGevuAgkVfdyCdtZnQTGWVVvR5Q",
+    "credentialSubject": {
+        "name": "James Chartrand",
+        "description": "James's article. "
+    },
+    "proof": {
+        "type": "DataIntegrityProof",
+        "created": "2025-08-25T19:28:22Z",
+        "verificationMethod": "did:key:z6MknNQD1WHLGGraFi6zcbGevuAgkVfdyCdtZnQTGWVVvR5Q#z6MknNQD1WHLGGraFi6zcbGevuAgkVfdyCdtZnQTGWVVvR5Q",
+        "cryptosuite": "eddsa-rdfc-2022",
+        "proofPurpose": "assertionMethod",
+        "proofValue": "z55KdEZMVuxVgDvhoFz8h4YC7nTqKMYuM7Fy4C1Qpff2TU9A3DMAmMM32Xkzdix6sYa5dhq7XLPHceYJz6pRz1Wmf"
+    }
+}`
