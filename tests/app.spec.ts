c049b2835e6137ccbbbf032f48736b71b9b85613
James Chartrand <jc.chartrand@gmail.com>
2025-08-29 13:09:34 -0400

    move local vc test into own file

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 import testVC from "./testVC";
-import { LogId, LogMessages } from "components/ResultLog/ResultLog";
+import { TestId, LogMessages } from "components/ResultLog/ResultLog";
 
 const testVCs =
   [
@@ -52,40 +52,3 @@ testVCs.forEach(({ name, vc, expected }) => {
 });
 
 
-test("local vc", async ({ page }) => {
-  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto("/");
-  // Find an element with the text 'About' and click on it
-  //  await page.click("text=About");
-  // The new URL should be "/about" (baseURL is used there)
-  //  await expect(page).toHaveURL("/about");
-  await expect(page.locator("h1")).toContainText("VerifierPlus");
-  await page.getByTestId('vc-text-area').fill(testVC)
-  //await page.getByTestId('verify-btn').click()
-  await page.getByRole('button', { name: 'Verify' }).click()
-  await expect(page.getByText(LogMessages.NoExpirationDate)).toBeVisible();
-});
-
-const logTests = [
-  {
-    name: 'legacy-noStatus',
-    vc: 'https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v1/dataIntegrityProof/didKey/legacy-noStatus-noExpiry.json',
-    expected: { expiry: LogMessages.NoExpirationDate }
-  },
-  {
-    name: 'has expired',
-    vc: 'https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v2/ed25519/didWeb/legacy-revokedStatus-expired.json',
-    expected: { expiry: LogMessages.HasExpired }
-  },
-
-]
-
-logTests.forEach(({ name, vc, expected }) => {
-  test(`log: ${name}`, async ({ page }) => {
-    await page.goto("/")
-    await page.getByTestId('vc-text-area').fill(vc)
-    await page.getByRole('button', { name: 'Verify' }).click()
-    await expect(page.getByTestId(`${LogId.Expiration}-msg`)).toHaveText(expected.expiry)
-    await expect(page.getByText(expected.expiry)).toBeVisible();
-  })
-});
