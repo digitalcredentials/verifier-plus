36bb514132952abc417f983b20e9d614a4c631e3
James Chartrand <jc.chartrand@gmail.com>
2025-08-29 15:37:30 -0400

    create general tests file

---
+++ tests/general.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect } from "@playwright/test";
+import { TestId, LogMessages } from "components/ResultLog/ResultLog";
+
+const logTests = [
+  {
+    name: 'legacy-noStatus-noExpiry',
+    vc: 'https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v1/dataIntegrityProof/didKey/legacy-noStatus-noExpiry.json',
+    expected:  [
+        { testId: TestId.ExpirationLogMsg, expectedText: LogMessages.NoExpirationDate },
+        { testId: TestId.RevocationLogMsg, expectedText: LogMessages.NotRevoked },
+        { testId: TestId.SigningLogMsg, expectedText: LogMessages.ValidSignature },
+        { testId: TestId.MalformedLogMsg, expectedText: LogMessages.WellFormed },
+        { testId: TestId.IssuerLogMsg, expectedText: LogMessages.KnownIssuer }
+
+    ] 
+  },
+  {
+    name: 'legacy-revoked-expired',
+    vc: 'https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v2/ed25519/didWeb/legacy-revokedStatus-expired.json',
+    expected: [
+        { testId: TestId.ExpirationLogMsg, expectedText: LogMessages.HasExpired },
+        { testId: TestId.RevocationLogMsg, expectedText: LogMessages.Revoked },
+        { testId: TestId.SigningLogMsg, expectedText: LogMessages.ValidSignature },
+        { testId: TestId.MalformedLogMsg, expectedText: LogMessages.WellFormed },
+        { testId: TestId.IssuerLogMsg, expectedText: LogMessages.KnownIssuer }
+    ]
+  }
+]
+
+// NOTE: these tests paste the url for the VC into V+, 
+// The tests in app.spec.ts retrieve the json and paste that.
+logTests.forEach(({ name, vc, expected }) => {
+  test(`log: ${name}`, async ({ page }) => {
+    await page.goto("/")
+    await page.getByTestId('vc-text-area').fill(vc)
+    await page.getByRole('button', { name: 'Verify' }).click()
+    for (let i = 0; i < expected.length; i++) {
+      const logTest = expected[i];
+      await expect(page.getByText(logTest.expectedText)).toBeVisible();
+      await expect(page.getByTestId(logTest.testId)).toHaveText(logTest.expectedText)
+    }
+  })
+});
