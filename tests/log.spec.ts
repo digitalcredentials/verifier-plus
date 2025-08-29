1bd7ae1fadd2306c898167ac3a8345e55345b86a
James Chartrand <jc.chartrand@gmail.com>
2025-08-29 13:05:34 -0400

    move verification log tests into own file

--- tests/log.spec.ts
+++ tests/log.spec.ts
@@ -1,85 +1,33 @@
 import { test, expect } from "@playwright/test";
-import { getTamperedVC, getTamperedVCAsString } from "./testVC";
+import testVC from "./testVC";
 import { TestId, LogMessages } from "components/ResultLog/ResultLog";
 
-const baseExpectedLogMessages = {
-    expiry: LogMessages.HasNotExpired,
-    revocation: LogMessages.NotRevoked,
-    signature: LogMessages.ValidSignature,
-    issuer: LogMessages.KnownIssuer,
-    wellFormed: LogMessages.WellFormed
-}
-
 const logTests = [
-    {
-        name: 'legacy-noStatus-noExpiry',
-        vc: 'https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v1/dataIntegrityProof/didKey/legacy-noStatus-noExpiry.json',
-        expected: { expiry: LogMessages.NoExpirationDate }
-    },
-    {
-        name: 'legacy-revoked-expired',
-        vc: 'https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v2/ed25519/didWeb/legacy-revokedStatus-expired.json',
-        expected: { expiry: LogMessages.HasExpired, revocation: LogMessages.Revoked }
-    },
-    {
-        name: 'oidf-noStatus-expired',
-        vc: 'https://github.com/digitalcredentials/vc-test-fixtures/raw/refs/heads/main/verifiableCredentials/v1/bothSignatureTypes/didKey/oidf-noStatus-expired.json',
-        expected: { expiry: LogMessages.HasExpired, revocation: LogMessages.NotRevoked }
-    },
-    {
-        name: 'noRegistry-noStatus-noExpiry',
-        vc: 'https://github.com/digitalcredentials/vc-test-fixtures/raw/refs/heads/main/verifiableCredentials/v1/bothSignatureTypes/didKey/noRegistry-noStatus-noExpiry.json',
-        expected: { expiry: LogMessages.NoExpirationDate, issuer: LogMessages.UnknownIssuer }
-    },
-    {
-        name: 'mixedRegistry-validStatus-noExpiry',
-        vc: 'https://github.com/digitalcredentials/vc-test-fixtures/raw/refs/heads/main/verifiableCredentials/v1/bothSignatureTypes/didKey/mixedRegistry-validStatus-noExpiry.json',
-        expected: { expiry: LogMessages.NoExpirationDate, revocation: LogMessages.NotRevoked }
-    },
-    {
-        name: 'mixedRegistry-validStatus-expired',
-        vc: 'https://github.com/digitalcredentials/vc-test-fixtures/raw/refs/heads/main/verifiableCredentials/v2/dataIntegrityProof/didKey/twoOIDF-validStatus-expired.json',
-        expected: { expiry: LogMessages.HasExpired, revocation: LogMessages.NotRevoked }
-    }
+  {
+    name: 'legacy-noStatus-noExpiry',
+    vc: 'https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v1/dataIntegrityProof/didKey/legacy-noStatus-noExpiry.json',
+    expected:  [
+      { testId: TestId.ExpirationLogMsg, expectedText: LogMessages.NoExpirationDate },
+      { testId: TestId.RevocationLogMsg, expectedText: LogMessages.NotRevoked }
+    ] 
+  },
+  {
+    name: 'legacy-revoked-expired',
+    vc: 'https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v2/ed25519/didWeb/legacy-revokedStatus-expired.json',
+    expected: [{ testId: TestId.ExpirationLogMsg, expectedText: LogMessages.HasExpired }]
+  }
 ]
-    
 
-// NOTE: these tests paste the url for the VC into V+, 
-// The tests in app.spec.ts retrieve the json and paste that.
+// NOTE: these tests pass the url for the VC to V+, whereas the other tests above go get the text and paste that in.
 logTests.forEach(({ name, vc, expected }) => {
-    test(`log: ${name}`, async ({ page }) => {
-        const expectedMessages = { ...baseExpectedLogMessages, ...expected }
-        await page.goto("/")
-        await page.getByTestId('vc-text-area').fill(vc)
-        await page.getByRole('button', { name: 'Verify' }).click()
-        // check well formed message matches expected
-        await expect(page.getByText(expectedMessages.wellFormed)).toBeVisible();
-        await expect(page.getByTestId(TestId.MalformedLogMsg)).toHaveText(expectedMessages.wellFormed)
-        // check signature message matches expected
-        await expect(page.getByText(expectedMessages.signature)).toBeVisible();
-        await expect(page.getByTestId(TestId.SigningLogMsg)).toHaveText(expectedMessages.signature)
-        // check issuer message matches expected
-        await expect(page.getByText(expectedMessages.issuer)).toBeVisible();
-        await expect(page.getByTestId(TestId.IssuerLogMsg)).toHaveText(expectedMessages.issuer)
-        // check revocation message matches expected
-        await expect(page.getByText(expectedMessages.revocation)).toBeVisible();
-        await expect(page.getByTestId(TestId.RevocationLogMsg)).toHaveText(expectedMessages.revocation)
-        // check expired message matches expected
-        await expect(page.getByText(expectedMessages.expiry)).toBeVisible();
-        await expect(page.getByTestId(TestId.ExpirationLogMsg)).toHaveText(expectedMessages.expiry)
-        // finally take a screenshot of the log area to capture the combination of checkmarks and x's
-        // NOTE: it is very important that this screenshot be taken when the app is working/displaying correctly
-        // SEE: https://playwright.dev/docs/test-snapshots
-        await expect(page.getByTestId(TestId.ResultLog)).toHaveScreenshot();
-    })
-});
-
-// errors that prevent the log from being shown
-test('tampered', async ({ page }) => {
-    const tamperedVC = getTamperedVCAsString()
+  test(`log: ${name}`, async ({ page }) => {
     await page.goto("/")
-    await page.getByTestId('vc-text-area').fill(tamperedVC)
+    await page.getByTestId('vc-text-area').fill(vc)
     await page.getByRole('button', { name: 'Verify' }).click()
-    await expect(page.getByTestId(TestId.GeneralErrorMsg)).toHaveText(LogMessages.GeneralError);
-})
-
+    for (let i = 0; i < expected.length; i++) {
+      const logTest = expected[i];
+      await expect(page.getByText(logTest.expectedText)).toBeVisible();
+      await expect(page.getByTestId(`${logTest.testId}-msg`)).toHaveText(logTest.expectedText)
+    }
+  })
+});
