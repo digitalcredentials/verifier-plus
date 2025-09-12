40e2cc396d09fc5779ba2c4881478f8be4bf5c83
James Chartrand <jc.chartrand@gmail.com>
2025-09-12 15:46:17 -0400

    move public cred header and footer into separate layout

---
+++ app/credentials/[publicCredentialId]/layout.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { BottomBar } from "@/components/BottomBar/BottomBar"
+import { TopBar } from "@/components/TopBar/TopBar"
+import styles from './[publicCredentialId].module.css'
+import { useState } from "react"
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+
+    const [isDark, setIsDark] = useState(false);
+
+  return (
+
+        <main className={styles.container}>
+             <TopBar hasLogo={true} isDark={isDark} setIsDark={setIsDark} />
+                  {children}
+            <BottomBar isDark={isDark} />
+           </main>
+
+  )
+}
