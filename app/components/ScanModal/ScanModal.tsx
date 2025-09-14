a0a1926e3d5078d1cbf9a7b15811955387cec1ea
James Chartrand <jc.chartrand@gmail.com>
2025-09-14 17:50:45 -0400

    replace old incompatible scanner and rework accordingly

--- app/components/ScanModal/ScanModal.tsx
+++ app/components/ScanModal/ScanModal.tsx
@@ -1,12 +1,70 @@
 import { Button } from '@/components/Button/Button';
 import type { ScanModalProps } from './ScanModal.d';
 import styles from './ScanModal.module.css';
+import { useEffect, useRef, useState } from "react";
 //import { QrReader } from 'react-qr-reader';
-import { Result } from '@zxing/library';
+import QrScanner from 'qr-scanner';
+//import { Result } from '@zxing/library';
 
 export const ScanModal = ({ isOpen, setIsOpen, onScan, setErrorMessage }: ScanModalProps) => {
+
+    let scanner : any;
+    const videoEl = useRef<HTMLVideoElement>(null);
+    const [qrOn, setQrOn] = useState<boolean>(true);
+
+    const onScanSuccess = (result: QrScanner.ScanResult) => {
+        console.log(result);
+        onScan(result?.data)
+        closeModal();
+    };
+
+    const onScanFail = (err: string | Error) => {
+        // setErrorMessage(true)
+        console.log(err);
+       // closeModal();
+    };
+
+    useEffect(() => {
+      
+        if (isOpen && videoEl?.current && !scanner) {
   
-  const handleScan = (newData?: Result | null, error?: Error | null) => {
+            scanner = new QrScanner(videoEl?.current, onScanSuccess, {
+                onDecodeError: onScanFail,
+                preferredCamera: "environment",
+                highlightScanRegion: true,
+                highlightCodeOutline: true
+            });
+            scanner?.start()
+                .then(() => setQrOn(true))
+                .catch((err:any) => {
+                    if (err) setQrOn(false);
+                    setErrorMessage(true)
+                });
+        }
+        if (!isOpen) {
+          scanner?.stop();
+        }
+
+        // Clean up on unmount.
+        // Removes the QR Scanner from rendering and using camera when it is closed or removed from the UI.
+        return () => {
+            if (!videoEl?.current) {
+                scanner?.stop();
+            }
+        };
+    }, [isOpen]);
+
+    // If "camera" is not allowed in browser permissions, show an alert.
+    useEffect(() => {
+        if (!qrOn)
+            alert(
+                "Camera is blocked or not accessible. Please allow camera in your browser permissions and Reload."
+            );
+    }, [qrOn]);
+
+
+
+/*   const handleScan = (newData?: Result | null, error?: Error | null) => {
     if (newData){
       const res = onScan(newData.getText());
       closeModal();
@@ -18,10 +76,11 @@ export const ScanModal = ({ isOpen, setIsOpen, onScan, setErrorMessage }: ScanMo
     if (error){
       console.log(error);
     }
-  }
+  } */
  
   function closeModal() {
     setIsOpen(false);
+
   }
 
   return (
@@ -38,6 +97,9 @@ export const ScanModal = ({ isOpen, setIsOpen, onScan, setErrorMessage }: ScanMo
                 </button>
               </div>
               <div className={styles.cameraContainer}>
+                <div className="qr-reader">
+                 <video className={styles.qrReader} ref={videoEl}></video>
+                 </div>
               {/*  <QrReader 
                   onResult={(result, error) => handleScan(result, error)}
                   constraints={{facingMode: "environment"}}
