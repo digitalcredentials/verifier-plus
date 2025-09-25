7ab5d68880f5a8ee1d74946e26b3175ca45e9721
James Chartrand <jc.chartrand@gmail.com>
2025-09-25 07:06:39 -0400

    add testids to info blocks

--- app/components/InfoBlock/InfoBlock.tsx
+++ app/components/InfoBlock/InfoBlock.tsx
@@ -1,11 +1,12 @@
 import type { InfoBlockProps } from './InfoBlock.d';
 import styles from './InfoBlock.module.css';
 
-export const InfoBlock = ({header, contents}: InfoBlockProps) => {
+export const InfoBlock = ({header, contents, testId}: InfoBlockProps) => {
+  // The testId allows playwright to more reliably find this element on the page
   return (
     <div className={styles.infoBlock}>
       <h2 className={styles.smallHeader}>{header}</h2>
-      <div className={styles.contents}>{contents}</div>
+      <div className={styles.contents} data-testid={testId}>{contents}</div>
     </div>
   );
 }
