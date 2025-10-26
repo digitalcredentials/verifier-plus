ba433be0f2a23d122b1967432205e24d0e5b843a
James Chartrand <jc.chartrand@gmail.com>
2025-10-26 14:18:32 -0400

    make help clickably toggled

--- app/components/ContextualHelp/ContextualHelp.tsx
+++ app/components/ContextualHelp/ContextualHelp.tsx
@@ -1,14 +1,37 @@
 import type { ContextualHelpProps } from './ContextualHelp.d';
-import {useFloating} from '@floating-ui/react';
-
+import {useFloating, useClick, useInteractions} from '@floating-ui/react';
+import { useState } from 'react';
 export const ContextualHelp = ({text}: ContextualHelpProps) => {
-  const {refs, floatingStyles} = useFloating();
+
+  const [isOpen, setIsOpen] = useState(false);
+ 
+  const {refs, floatingStyles, context} = useFloating({
+    open: isOpen,
+    onOpenChange: setIsOpen,
+  });
+ 
+  const click = useClick(context);
+ 
+  const {getReferenceProps, getFloatingProps} = useInteractions([
+    click,
+  ]);
+ 
   return (
     <>
-      <button ref={refs.setReference}>Help!</button>
-      <div ref={refs.setFloating} style={floatingStyles}>
-        {text}
+      <div ref={refs.setReference} {...getReferenceProps()}>
+        <button ref={refs.setReference}>Help!</button>
+       
       </div>
+      {isOpen && (
+        <div
+          ref={refs.setFloating}
+          style={floatingStyles}
+          {...getFloatingProps()}
+        >
+          Floating element
+        </div>
+      )}
     </>
   );
+
 }
