5a24be5aae61452b166e39ec2976386682a4ed14
James Chartrand <jc.chartrand@gmail.com>
2025-10-27 09:03:11 -0400

    replace @floating-ui/react with @base-ui-components/react

--- app/components/ContextualHelp/ContextualHelp.tsx
+++ app/components/ContextualHelp/ContextualHelp.tsx
@@ -1,39 +1,35 @@
+import * as React from 'react';
 import type { ContextualHelpProps } from './ContextualHelp.d';
-import {useFloating, useClick, useInteractions} from '@floating-ui/react';
-import { useState } from 'react';
 import HelpIcon from '@mui/icons-material/HelpOutlined';
-import styles from './ContextualHelp.module.css';
+import { Dialog } from '@base-ui-components/react/dialog';
+import styles from './ContextualHelp.module.css'
+
 
 export const ContextualHelp = ({text}: ContextualHelpProps) => {
-
-  const [isOpen, setIsOpen] = useState(false);
- 
-  const {refs, floatingStyles, context} = useFloating({
-    open: isOpen,
-    onOpenChange: setIsOpen,
-  });
- 
-  const click = useClick(context);
- 
-  const {getReferenceProps, getFloatingProps} = useInteractions([
-    click,
-  ]);
- 
   return (
-    <>
-      <span className={styles.icon} ref={refs.setReference} {...getReferenceProps()}>
-       <HelpIcon fontSize='inherit' color='info'/>
-      </span>
-      {isOpen && (
-        <div
-          ref={refs.setFloating}
-          style={floatingStyles}
-          {...getFloatingProps()}
-        >
-          {text}
-        </div>
-      )}
-    </>
+    <Dialog.Root>
+      <Dialog.Trigger nativeButton={false} render={<span className={styles.icon}><HelpIcon fontSize='inherit' color='info'/></span>}>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Backdrop className="fixed inset-0 min-h-dvh bg-black opacity-20 transition-all duration-150 data-[ending-style]:opacity-0 data-[starting-style]:opacity-0 dark:opacity-70 supports-[-webkit-touch-callout:none]:absolute" />
+        <Dialog.Popup className="fixed top-1/2 left-1/2 -mt-8 w-96 max-w-[calc(100vw-3rem)] -translate-x-1/2 -translate-y-1/2 rounded-lg bg-gray-50 p-6 text-gray-900 outline outline-1 outline-gray-200 transition-all duration-150 data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0 dark:outline-gray-300">
+          <Dialog.Title className="-mt-1.5 mb-1 text-lg font-medium">Notifications</Dialog.Title>
+          <Dialog.Description className="mb-6 text-base text-gray-600">
+            You are all caught up. Good job!
+          </Dialog.Description>
+          <div className="flex justify-end gap-4">
+            <Dialog.Close className="flex h-10 items-center justify-center rounded-md border border-gray-200 bg-gray-50 px-3.5 text-base font-medium text-gray-900 select-none hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:-outline-offset-1 focus-visible:outline-blue-800 active:bg-gray-100">
+              Close
+            </Dialog.Close>
+          </div>
+        </Dialog.Popup>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
-
 }
+
+       
+    
+
+
+
