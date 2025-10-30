98e52862197cad2b3474a53079b80dcae1d8d4f9
James Chartrand <jc.chartrand@gmail.com>
2025-10-30 11:58:03 -0400

    extract help content to @digitalcredentials/vc-help-react

--- app/components/Issuer/Issuer.tsx
+++ app/components/Issuer/Issuer.tsx
@@ -3,20 +3,10 @@ import type { IssuerProps } from './Issuer.d';
 import styles from './Issuer.module.css';
 import { TestId } from '@/lib/testIds';
 import { ContextualHelp } from '../ContextualHelp/ContextualHelp';
+import { IssuerHelp } from '@digitalcredentials/vc-help-react';
 
 export const Issuer = ({ issuer, header, infoButtonPushed }: IssuerProps) => {
   const issuerImage = useRef<HTMLImageElement>(null);
-
-  const HelpText = () => {
-    return <>
-      The issuer is the person or institution that issued the Verifiable Credential. 
-      This might be, for example, a university that has granted a degree to a student
-      and subsequently then issued a Verifiable Credential attesting that the student did earn the degree.
-      <img
-        src="/LightModeLogo.png"
-        alt="DCC logo" />
-    </>
-  }
 
   const handleonError = () => {
     if (issuerImage.current != null) {
@@ -28,7 +18,7 @@ export const Issuer = ({ issuer, header, infoButtonPushed }: IssuerProps) => {
     <div>
       {(issuer?.image || issuer?.name || issuer?.url) && (
         <div>
-          <h2 className={styles.header}>{header}<ContextualHelp title="What's an issuer?"><HelpText/></ContextualHelp></h2>
+          <h2 className={styles.header}>{header}<ContextualHelp title="What's an issuer?"><IssuerHelp/></ContextualHelp></h2>
           <div className={styles.issuer}>
             {issuer.image && (
               <img src={issuer.image?.id || issuer.image} width={36} height={36} alt={`${issuer.name} logo`} ref={issuerImage} onError={handleonError} />
