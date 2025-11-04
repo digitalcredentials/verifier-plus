1145cd73e326210f592e636d9c4b2f5bcab56527
James Chartrand <jc.chartrand@gmail.com>
2025-11-04 13:14:42 -0500

    add resultItem typescript types

--- app/components/ResultLog/ResultLog.d.ts
+++ app/components/ResultLog/ResultLog.d.ts
@@ -3,3 +3,15 @@ import { VerifyResponse } from "types/credential";
 export type ResultLogProps = {
   verificationResult: VerifyResponse;
 }
+
+export type ResultItem = {
+    verified:boolean,
+    positiveMessage:string,
+    negativeMessage?:string,
+    warningMessage?:string,
+    sourceLogId?:string,
+    testId:string,
+    helpTitle?:string, 
+    HelpContent?:ReactElement,
+    issuer?:boolean
+}
