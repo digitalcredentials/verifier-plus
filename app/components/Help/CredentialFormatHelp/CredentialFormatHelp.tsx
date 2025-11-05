9364ceee0159c621dda5e68dc24d49bdf6c20713
James Chartrand <jc.chartrand@gmail.com>
2025-11-05 15:26:22 -0500

    fix format help styling

--- app/components/Help/CredentialFormatHelp/CredentialFormatHelp.tsx
+++ app/components/Help/CredentialFormatHelp/CredentialFormatHelp.tsx
@@ -6,16 +6,16 @@ export const CredentialFormatHelp = () => {
       <li>These aren't checks on the validity of the signature or data, but rather simply that the data is in the format we expect.</li>
     </ul>
 
-    <p className={styles.note}><img src="./icons/info_24_lime.svg"></img>We try to check for as many things as possible, but 
-    of course there may be edge cases we haven't yet surfaced.
-    </p>
-    <p  className={styles.criteria}><h1 className={styles.title}>How we determine the validity</h1>
+    <div className={styles.note}><img className={styles.infoIcon} src="./icons/info_24_lime.svg"></img>We try to check for as many things as possible, but 
+    of course there may be edge cases that haven't yet surfaced.
+    </div>
+    <div  className={styles.criteria}><div className={styles.criteriaTitle}>How we determine the credential is properly formatted</div>
         <div className={styles.preference}>The credential must:</div>
       <ul className={styles.list}>
       <li>be valid JSON</li>
       <li>have a JSON-LD context</li>
     </ul>
-    </p>
+    </div>
 
     
   </>
